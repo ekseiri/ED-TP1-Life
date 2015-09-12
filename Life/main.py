5e3391682a97a0819728a1c159c2a8fe826e745f
Emiliano Kseiri <ekseiriar@gmail.com>
2015-09-12 01:56:13 -0300

    Menu

--- Life/main.py
+++ Life/main.py
@@ -5,19 +5,22 @@ def menu_main():
     print ('1.- Nuevo Juego')
     print ('2.- Cargar')
     print ('3.- Salir')
-    print ('\n' + 'Seleccion: ')
+    return raw_input('\n' + 'Seleccion: ')
+
 
 def menu_game_modes():
     """Menu de seleccion de modo de juego"""
-    
+
     print ('1.- Normal')
     print ('2.- Vidas Estaticas')
     print ('\n' + 'Seleccion: ')
+
 
 def menu_load():
     """Menu de carga de un juego empezado"""
 
     pass
+
 
 def main():
     menu_main()
--- Life/tablero.py
+++ Life/tablero.py
@@ -1,20 +1,30 @@
 import random
 
+
 class Tablero:
 
     def __init__(self, col, row, vivas):
         self.vivas = vivas
-        self.col = col
-        self.row = row
-        self.tablero = [[None] * self.col for self.row in range(self.row) ]
+        self._col = col
+        self._row = row
+        self._tablero = [[None] * self._col for self._row in range(self._row)]
 
     def llenar(self):
-        print (self.vivas, self.col, self.row)
+        print (self.vivas, self._col, self._row)
         for i in range(self.vivas):
-            posicion = (random.randint(0,self.row),random.randint(0,self.col - 1))
+            posicion = (random.randint(0, self._row),
+                        random.randint(0, self._col - 1))
 
             while (self.tablero[posicion[0]][posicion[1]] is not None):
-                posicion = (random.randint(0,self.row),random.randint(0,self.col - 1))
+                posicion = (random.randint(0, self._row),
+                            random.randint(0, self._col - 1))
             else:
                 self.tablero[posicion[0]][posicion[1]] = 1
 
+    @property
+    def tablero(self):
+        return self._tablero
+
+    @tablero.setter
+    def tablero(self, t):
+        self._tablero = t
