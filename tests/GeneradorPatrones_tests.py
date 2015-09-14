d4cfe1eca9ec9f0019ce110486648149d1ea7669
Catero <cybercat_666_78@hotmail.com>
2015-09-14 11:07:18 -0300

    GeneradorPatrones_tests General Fixing

--- tests/GeneradorPatrones_tests.py
+++ tests/GeneradorPatrones_tests.py
@@ -24,9 +24,10 @@ def test_cantidad_de_vecinos_correcta():
     assert_equal(GeneradorPatrones.vecinos(t._tablero, 0, 0), 0)
     assert_equal(GeneradorPatrones.vecinos(t._tablero, 1, 2), 2)
     assert_equal(GeneradorPatrones.vecinos(t._tablero, 1, 3), 2)
+    assert_equal(GeneradorPatrones.vecinos(t._tablero, 2, 2), 6)
     assert_equal(GeneradorPatrones.vecinos(t._tablero, 2, 3), 4)
     assert_equal(GeneradorPatrones.vecinos(t._tablero, 3, 1), 2)
-    assert_equal(GeneradorPatrones.vecinos(t._tablero, 3, 2), 2)
+    assert_equal(GeneradorPatrones.vecinos(t._tablero, 3, 2), 3)
     assert_equal(GeneradorPatrones.vecinos(t._tablero, 3, 3), 2)
     assert_equal(GeneradorPatrones.vecinos(t._tablero, 4, 0), 1)
 
@@ -36,18 +37,31 @@ def test_avance_correcto_del_patron():
     proximo = GeneradorPatrones.nextStep(t._tablero)
     assert_equal(proximo, [[0, 0, 0, 0, 0],
                            [0, 0, 1, 1, 0],
-                           [0, 1, 1, 0, 1],
+                           [0, 1, 0, 0, 1],
                            [0, 1, 1, 1, 0],
                            [0, 1, 1, 0, 0]])
 
     proximo = GeneradorPatrones.nextStep(proximo)
     assert_equal(proximo, [[0, 0, 0, 0, 0],
-                           [0, 1, 1, 1, 0],
-                           [0, 0, 0, 0, 1],
-                           [1, 0, 0, 0, 0],
+                           [0, 0, 1, 1, 0],
+                           [0, 1, 0, 0, 1],
+                           [1, 0, 0, 1, 0],
                            [0, 1, 0, 1, 0]])
 
     proximo = GeneradorPatrones.nextStep(proximo)
+    assert_equal(proximo, [[0, 0, 0, 0, 0],
+                           [0, 0, 1, 1, 0],
+                           [0, 1, 0, 0, 1],
+                           [1, 1, 0, 1, 1],
+                           [0, 0, 1, 0, 0]])
+
+    t2 = [[0, 0, 0, 0, 0],
+          [0, 1, 1, 1, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0]]
+
+    proximo = GeneradorPatrones.nextStep(t2)
     assert_equal(proximo, [[0, 0, 1, 0, 0],
                            [0, 0, 1, 0, 0],
                            [0, 0, 1, 0, 0],
@@ -58,5 +72,18 @@ def test_avance_correcto_del_patron():
     assert_equal(proximo, [[0, 0, 0, 0, 0],
                            [0, 1, 1, 1, 0],
                            [0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0]])
+
+    t3 = [[0, 0, 0, 0, 0],
+          [0, 0, 1, 1, 0],
+          [0, 0, 1, 1, 0],
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0]]
+
+    proximo = GeneradorPatrones.nextStep(t3)
+    assert_equal(proximo, [[0, 0, 0, 0, 0],
+                           [0, 0, 1, 1, 0],
+                           [0, 0, 1, 1, 0],
                            [0, 0, 0, 0, 0],
                            [0, 0, 0, 0, 0]])
