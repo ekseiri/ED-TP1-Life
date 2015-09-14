f67296764ab8b3e242fcab4a6b8bf1dd3ce00a16
Emiliano Kseiri <ekseiriar@gmail.com>
2015-09-13 22:21:35 -0300

    fix

--- tests/GeneradorPatrones_tests.py
+++ tests/GeneradorPatrones_tests.py
@@ -21,19 +21,19 @@ def teardown():
 
 @with_setup(setup)
 def test_cantidad_de_vecinos_correcta():
-    assert_equal(GeneradorPatrones.vecinos(t, 0, 0), 0)
-    assert_equal(GeneradorPatrones.vecinos(t, 1, 2), 2)
-    assert_equal(GeneradorPatrones.vecinos(t, 1, 3), 2)
-    assert_equal(GeneradorPatrones.vecinos(t, 2, 3), 4)
-    assert_equal(GeneradorPatrones.vecinos(t, 3, 1), 2)
-    assert_equal(GeneradorPatrones.vecinos(t, 3, 2), 2)
-    assert_equal(GeneradorPatrones.vecinos(t, 3, 3), 2)
-    assert_equal(GeneradorPatrones.vecinos(t, 4, 0), 1)
+    assert_equal(GeneradorPatrones.vecinos(t._tablero, 0, 0), 0)
+    assert_equal(GeneradorPatrones.vecinos(t._tablero, 1, 2), 2)
+    assert_equal(GeneradorPatrones.vecinos(t._tablero, 1, 3), 2)
+    assert_equal(GeneradorPatrones.vecinos(t._tablero, 2, 3), 4)
+    assert_equal(GeneradorPatrones.vecinos(t._tablero, 3, 1), 2)
+    assert_equal(GeneradorPatrones.vecinos(t._tablero, 3, 2), 2)
+    assert_equal(GeneradorPatrones.vecinos(t._tablero, 3, 3), 2)
+    assert_equal(GeneradorPatrones.vecinos(t._tablero, 4, 0), 1)
 
 
 @with_setup(setup)
 def test_avance_correcto_del_patron():
-    proximo = GeneradorPatrones.nextStep(t)
+    proximo = GeneradorPatrones.nextStep(t._tablero)
     assert_equal(proximo, [[0, 0, 0, 0, 0],
                            [0, 0, 1, 1, 0],
                            [0, 1, 1, 0, 1],
