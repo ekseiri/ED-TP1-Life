82e45af016664c28d400b1637d2daf77cabcdf83
Catero <cybercat_666_78@hotmail.com>
2015-09-14 01:18:36 -0300

    Update GeneradorPatrones.py

--- Life/GeneradorPatrones.py
+++ Life/GeneradorPatrones.py
@@ -4,35 +4,53 @@ class GeneradorPatrones(object):
         pass
 
     @staticmethod
-    def vecinos(tablero,x,y):
+    def vecinos(tablero,fila,columna):
+        if fila == 0:
+            rangoFilai = 0
+        else:
+            rangoFilai = fila-1
+
+        if columna == 0:
+            rangoColumnai = 0
+        else:
+            rangoColumnai = columna-1
+
+        if (fila + 1) > len(tablero):
+            rangoFilaf = fila
+        else:
+            rangoFilaf = fila + 1
+
+        if (columna + 1) > len(tablero[0]):
+            rangoColumnaf = columna
+        else:
+            rangoColumnaf = columna + 1
+
         vivos=0
-        for i in range(3):
-            for j in range(3):
-                try:
-                    if tablero[x-1+i][y-1+j]==1:
-                        vivos=+1
-                except Exception:
-                    pass
-        print(x,y)
-        if tablero[x][y]==1:
+        for i in range(rangoFilai,rangoFilaf):
+            for j in range(rangoColumnai,rangoColumnaf):
+                if tablero[i][j]==1:
+                    vivos=+1
+                
+        if tablero[fila][columna]==1:
             return vivos-1
         return vivos
 
     @staticmethod
     def nextStep(tablero):
         modificado = list(tablero)
-        for y in range(len(tablero)):
-            for x in range(len(tablero[y])):
-                vecinos = self.vecinos(tablero,x,y)
-                if tablero[x][y]==1:
+
+        for fila in range(len(tablero)):
+            for columna in range(len(tablero[0])):
+                vecinos = GeneradorPatrones.vecinos(tablero,fila,columna)
+                if tablero[fila][columna]==1:
                     if vecinos==2 or vecinos==3:
-                        modificado[x][y] = 1
+                        modificado[fila][columna] = 1
                     else:
-                        modificado[x][y] = 0
+                        modificado[fila][columna] = 0
                 else:
                     if vecinos==3:
-                        modificado[x][y] = 1 
+                        modificado[fila][columna] = 1 
                     else:
-                        modificado[x][y] = 0
+                        modificado[fila][columna] = 0
         return modificado
                 
